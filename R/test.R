9246d74588b9a8f7a130b7a8a11178f4e6ede155
Seda Erdem <se17@stir.ac.uk>
2019-07-02 16:56:24 +0100

    test mini code

--- R/test.R
+++ R/test.R
@@ -1,4 +1,5 @@
-# Adding some codes
-x <- 1:3
-
+setwd(getwd() )  
+album <- read.csv(file= "data/album_sales.csv", header = TRUE)
+plot(Sales~Adverts, data=album)
+abline(lm(Sales ~ Adverts, data=album), col="blue", lwd=3, lty=2)
 
